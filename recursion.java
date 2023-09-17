ca1a098962ac0479b6cb8fb87ee76d314dff0074
saurabhgangal <gangal.s@northeastern.edu>
2023-09-17 00:55:54 -0400

    Create recursion.java

---
+++ recursion.java
@@ -0,0 +1,20 @@
+import java.util.*;
+ class recursion {
+public static Scanner scn = new Scanner(System.in);
+
+public static int[] joker(){
+    return null;
+}
+
+
+
+
+public static void Main(String []args){
+
+}
+
+
+
+
+
+ }
